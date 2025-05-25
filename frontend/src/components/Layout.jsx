f8ad275f537cdfe029acd2ac2990410650a8bbee
Vishnu Reddy Palagiri <136872087+vishnujsf@users.noreply.github.com>
2025-05-25 21:33:46 +0530

    Update Layout.jsx
    5th commit

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,14 +3,14 @@ import Navbar from "./Navbar";
 
 const Layout = ({ children, showSidebar = false }) => {
   return (
-    <div className="min-h-screen">
-      <div className="flex">
+    <div className="min-h-screen flex flex-col">
+      <div className="flex flex-1">
         {showSidebar && <Sidebar />}
 
         <div className="flex-1 flex flex-col">
           <Navbar />
 
-          <main className="flex-1 overflow-y-auto">{children}</main>
+          <main className="flex-1">{children}</main>
         </div>
       </div>
     </div>
