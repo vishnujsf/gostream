c3b613c2510158613876d5193f1b5641d1a3dfd1
Vishnu Reddy Palagiri <136872087+vishnujsf@users.noreply.github.com>
2025-05-25 21:26:54 +0530

    Update Layout.jsx
    layout commit

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = ({ children, showSidebar = false }) => {
         <div className="flex-1 flex flex-col">
           <Navbar />
 
-          <main className="flex-1 overflow-y-auto">{children}</main>
+          <main className="flex-1 min-h-screen">{children}</main>
         </div>
       </div>
     </div>
