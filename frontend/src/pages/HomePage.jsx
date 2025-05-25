dedb9d35c7d295e5ec6c8db4eef01620b9a0c0ad
Vishnu Reddy Palagiri <136872087+vishnujsf@users.noreply.github.com>
2025-05-25 21:37:34 +0530

    Update HomePage.jsx
    6th

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -49,7 +49,7 @@ const HomePage = () => {
   }, [outgoingFriendReqs]);
 
   return (
-    <div className="p-4 sm:p-6 lg:p-8">
+    <div className="bg-base-200 min-h-screen p-4 sm:p-6 lg:p-8">
       <div className="container mx-auto space-y-10">
         <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
           <h2 className="text-2xl sm:text-3xl font-bold tracking-tight">Your Friends</h2>
